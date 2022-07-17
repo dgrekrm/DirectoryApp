ab03bf37054404f7addd27b15c4a4b66b070e6d1
Ekrem Değer <ekrem.deger@threadinmotion.com>
2022-07-17 13:10:03 +0300

    postgresql connection test

--- DirectoryApp.TestApplication/Program.cs
+++ DirectoryApp.TestApplication/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Npgsql;
+
+var cs = @"Server=127.0.0.1;Port=5432;Database=testdatabase;User Id=postgres;Password=Qasx7865";
+
+using var con = new NpgsqlConnection(cs);
+con.Open();
+
+var sql = "SELECT NOW()";
+
+using var cmd = new NpgsqlCommand(sql, con);
+
+var date = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"PostgreSQL version: {date}");
+
+Console.ReadLine();
